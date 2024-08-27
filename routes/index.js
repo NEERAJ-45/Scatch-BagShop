b428263f39ca133507e33cf4be9b6bdb9362e4dd
neeraj <neerajsurnis143r@gmail.com>
2024-08-27 21:02:50 +0530

    sjs

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../middlewares/isLoggedIn");
 const productModel = require("../models/productModel");
+const userModel = require("../models/userModel");
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
@@ -12,5 +13,10 @@ router.get("/shop", async (req, res) => {
   let products = await productModel.find();
 
   res.render("shop", { products });
+});
+router.get("/addtocart/:id", async (req, res) => {
+
+
+  let user  = await userModel.findOne({email: req.user.email});
 });
 module.exports = router;
